c8493db9196c3dbecee025047d0a682123c91495
Paata Pheikrishvili <paatapp@gmail.com>
2023-06-25 17:07:56 +0400

    Add extensions

---
+++ Movies.Domain/Shared/Extensions/ExceptionText.cs
@@ -0,0 +1,20 @@
+﻿namespace Movies.Domain.Shared.Extensions;
+
+public static class ExceptionText
+{
+    public static async Task<string?> ToErrorStr(this Exception? ex, CancellationToken token = default)
+    {
+        string errorStr = "";
+        Exception? tmpEx = ex;
+
+        await Task.Run(() =>
+        {
+            while (tmpEx != null)
+            {
+                errorStr += $"{tmpEx.Message}{Environment.NewLine}";
+                tmpEx = tmpEx.InnerException;
+            }
+        }, token).ConfigureAwait(false);
+        return errorStr;
+    }
+}
---
+++ Movies.Infrastructure/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Movies.Domain.Shared.Extensions;
+using Newtonsoft.Json;
+
+namespace Movies.Infrastructure.Middleware;
+
+public class GlobalExceptionHandler : IMiddleware
+{
+    private readonly ILogger<GlobalExceptionHandler>? _logger;
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler>? logger)
+    {
+        _logger = logger;
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception ex, ILogger<GlobalExceptionHandler>? logger)
+    {
+        string errorMessage = await ex.ToErrorStr() ?? "";
+
+        logger?.LogError($"Exception details: {errorMessage}" );
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        //await context.Response.WriteAsync(result).ConfigureAwait(false);
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(new { StatusCode = HttpStatusCode.InternalServerError,
+            Message = errorMessage })).ConfigureAwait(false);
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            //var F = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            _logger?.Log (LogLevel.Information,$"{await new StreamReader(context.Response.Body).ReadToEndAsync().ConfigureAwait(false)}");
+            await next(context);
+        }
+        catch (Exception exceptionObj)
+        {
+            await HandleExceptionAsync(context, exceptionObj, _logger).ConfigureAwait(false);
+        }
+    }
+}
