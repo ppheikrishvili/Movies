e506a0c39fa8c2aced27e36e152b8388e29e9c6a
Paata Pheikrishvili <paatapp@gmail.com>
2023-07-18 10:22:30 +0400

    Log request and respons

--- Movies.Infrastructure/Middleware/GlobalExceptionHandler.cs
+++ Movies.Infrastructure/Middleware/GlobalExceptionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Movies.Domain.Shared.Extensions;
@@ -33,11 +34,38 @@ public class GlobalExceptionHandler : IMiddleware
     {
         try
         {
+            string requestStr = await FormatRequest(context.Request);
+            using var responseBody = new MemoryStream();
+            Stream originalBodyStream = context.Response.Body;
+            context.Response.Body = responseBody;
             await next(context);
+            _logger?.Log(LogLevel.Information,
+                $"Requested - {requestStr} {Environment.NewLine} Response - {await FormatResponse(context.Response)}");
+            await responseBody.CopyToAsync(originalBodyStream);
         }
         catch (Exception exceptionObj)
         {
             await HandleExceptionAsync(context, exceptionObj, _logger).ConfigureAwait(false);
         }
+    }
+
+    private async Task<string> FormatResponse(HttpResponse response)
+    {
+        response.Body.Seek(0, SeekOrigin.Begin);
+        string text = await new StreamReader(response.Body).ReadToEndAsync();
+        response.Body.Seek(0, SeekOrigin.Begin);
+        return $"{response.StatusCode}: {text}";
+    }
+
+
+    private async Task<string> FormatRequest(HttpRequest request)
+    {
+        var body = request.Body;
+        request.EnableBuffering();
+        var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+        int _ = await request.Body.ReadAsync(buffer, 0, buffer.Length);
+        var bodyAsText = Encoding.UTF8.GetString(buffer);
+        request.Body = body;
+        return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
     }
 }
